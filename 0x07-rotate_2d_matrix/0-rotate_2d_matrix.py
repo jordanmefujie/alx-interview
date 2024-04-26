f589594208e16b81f8b7593366d6dd8f2c1695f9
jordanmefujie <mjordanlonpo@gmail.com>
2024-04-26 13:49:44 -0700

    Rotate 2D Matrix.

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,24 +6,22 @@
 def rotate_2d_matrix(matrix):
     """Rotates an m by n 2D matrix in place.
     """
-    if type(matrix) != list:
+    if not isinstance(matrix, list) or len(matrix) == 0 or not
+    all(isinstance(row, list) for row in matrix):
         return
-    if len(matrix) <= 0:
-        return
-    if not all(map(lambda x: type(x) == list, matrix)):
-        return
+
     rows = len(matrix)
     cols = len(matrix[0])
-    if not all(map(lambda x: len(x) == cols, matrix)):
+
+    if not all(len(row) == cols for row in matrix):
         return
-    c, r = 0, rows - 1
-    for i in range(cols * rows):
-        if i % rows == 0:
-            matrix.append([])
-        if r == -1:
-            r = rows - 1
-            c += 1
-        matrix[-1].append(matrix[r][c])
-        if c == cols - 1 and r >= -1:
-            matrix.pop(r)
-        r -= 1
+
+    new_matrix = [[0] * rows for _ in range(cols)]
+
+    for i in range(rows):
+        for j in range(cols):
+            new_matrix[j][rows - i - 1] = matrix[i][j]
+
+    for i in range(rows):
+        for j in range(cols):
+            matrix[i][j] = new_matrix[i][j]
